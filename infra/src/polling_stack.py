6c0f15aec7e80dfadfdee476ec48a512d13b7b37
Wes Drew <wesdrew@gmail.com>
2026-02-28 20:52:38 -0500

    try entry at the root of the project

--- infra/src/polling_stack.py
+++ infra/src/polling_stack.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 
-from aws_cdk import Duration, Stack, aws_lambda
-from aws_cdk.aws_lambda_python_alpha import PythonFunction
+from aws_cdk import DockerImage, Duration, Stack, aws_lambda
+from aws_cdk.aws_lambda_python_alpha import BundlingOptions, PythonFunction
 from constructs import Construct
 
 
@@ -13,8 +13,8 @@ class PollingStack(Stack):
         self.polling_fn = PythonFunction(
             self,
             id="Boston311PollingLambda",
-            entry="./polling",
-            index="app.py",
+            entry="polling",
+            index="src/app.py",
             handler="handler",
             runtime=aws_lambda.Runtime.PYTHON_3_12,
             timeout=Duration.seconds(300),
---
+++ synth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Generating requirements.txt for polling lambda"
+uv export --frozen --package polling --no-emit-workspace -o polling/requirements.txt > /dev/null
+
+echo "Running CDK" 
+if uv run python infra/src/app.py > /dev/null; then
+    echo "CDK synthesis successful!"
+else
+    echo "CDK failed! Check the errors above."
+    exit 1
+fi
+
+echo "Finished!" 
