b1b460af9fbafedb1600b7c40952434e37c9899a
Wes Drew <wesdrew@gmail.com>
2026-03-08 23:59:16 -0400

    add shared layer

--- synth.sh
+++ synth.sh
@@ -3,6 +3,9 @@
 echo "Generating requirements.txt for polling lambda"
 uv export --frozen --package polling --no-emit-workspace -o polling/requirements.txt > /dev/null
 
+echo "Generating requirements.txt for shared layer"
+uv export --frozen --package shared --no-emit-workspace -o shared/requirements.txt > /dev/null
+
 echo "Running CDK" 
 if uv run python infra/src/app.py > /dev/null; then
     echo "CDK synthesis successful!"
